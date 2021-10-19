b197058fbd82f2a4a6e9ea2d50e3b83ef25dff4a
diegoflores20173372 <40549405+diegoflores20173372@users.noreply.github.com>
2021-10-18 22:27:37 -0500

    View y ViewModel Parcial

---
+++ app/src/main/java/com/lab04/lab04_idnp/view/VisitFormActivity.kt
@@ -0,0 +1,12 @@
+package com.lab04.lab04_idnp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lab04.lab04_idnp.R
+
+class VisitFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visit_form)
+    }
+}
